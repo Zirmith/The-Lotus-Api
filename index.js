348355663b07fd436e7f19086baf58adb554f12b
ZDLua <hj_jr@icloud.com>
2023-03-01 17:47:51 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
+app.get('/api/home', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app;
+
